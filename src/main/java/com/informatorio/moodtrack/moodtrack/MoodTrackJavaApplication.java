bf665d232e587a1c4051c982d1f0c80c1ed921df
Alejandro Torres <at7709739@gmail.com>
2025-11-29 22:01:15 -0300

    Commit inicial

---
+++ src/main/java/com/informatorio/moodtrack/moodtrack/MoodTrackJavaApplication.java
@@ -0,0 +1,13 @@
+package com.informatorio.moodtrack.moodtrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoodTrackJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoodTrackJavaApplication.class, args);
+    }
+
+}
