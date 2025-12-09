6bf075ce99322fea3ecbc9e43e1024ddce8e06be
Alejandro Torres <at7709739@gmail.com>
2025-12-09 17:00:55 -0300

    feat: Implementación de manejo de errores de unicidad de base de datos

--- src/main/java/com/informatorio/moodtrack/moodtrack/exception/GlobalExceptionHandler.java
+++ src/main/java/com/informatorio/moodtrack/moodtrack/exception/GlobalExceptionHandler.java
@@ -3,6 +3,7 @@ package com.informatorio.moodtrack.moodtrack.exception;
 import com.informatorio.moodtrack.moodtrack.dto.errores.ApiError;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -40,5 +41,36 @@ public class GlobalExceptionHandler {
         return ResponseEntity.badRequest().body(apiError);
     }
 
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<ApiError> handleDataIntegrityViolationException(DataIntegrityViolationException ex
+            ,HttpServletRequest request) {
+        String specificMessage = "Error de integridad de datos";
+        String details = "Asegúrate de que los campos con restricción de unicidad (como Email) no estén duplicados.";
 
+        if (ex.getCause() != null && ex.getCause().getMessage().contains("Unique index or primary key violation")) {
+            specificMessage = "Violación de Unicidad";
+
+            String dbErrorDetails = ex.getCause().getMessage();
+
+            if (dbErrorDetails.contains("EMAIL")) {
+                details = "El Email proporcionado ya está registrado. Por favor, utiliza uno diferente.";
+            } else {
+                details = "El valor para un campo único (ej. ID o Email) ya existe.";
+            }
+        }
+
+        ApiError apiError = ApiError
+                .builder()
+                .timestamp(LocalDateTime.now())
+                .status(HttpStatus.CONFLICT.value())
+                .error(specificMessage)
+                .message(details)
+                .path(request.getRequestURI())
+                .validationErrors(null)
+                .build();
+
+        log.warn("Error de Integridad de Datos en {} : {}", request.getRequestURI(), ex.getMessage());
+
+        return new ResponseEntity<>(apiError, HttpStatus.CONFLICT);
+    }
 }
